8052fc521e96809ebae507545a7c13672f8574be
Jessica Castro <Jessica.castros@outlook.com>
2022-07-07 12:41:50 -0300

    Inserindo finalização dos exercícios de eventos e modificações via Javascript

--- instagram_base/js/register.js
+++ instagram_base/js/register.js
@@ -28,3 +28,62 @@ changeUsernameTagToStrong();
 // changePlaceholderInputName();
 changePlaceholderInputNameWithGetElementsByName();
 changeDocumentBackgroundColor();
+
+// const validateEmail = (event) => {
+//   event.preventDefault();
+
+//   const { value } = event.target;
+
+//   if (!value.includes('@') || !value.includes('.com') || value.length < 10) {
+//     console.log('Email inválido');
+//     event.target.style.border = '2px solid red';
+//   }
+// }
+// const emailInput = document.getElementById('email');
+// emailInput.style.outline = 'none';
+// emailInput.onfocus = (event) => emailInput.style.border = '2px solid green';
+// emailInput.onblur = (event) => validateEmail(event)
+// emailInput.onchange = (event) => validateEmail(event);
+
+const validate = ({ email, name, surname, username, password }) => {
+  if (!email.value.includes('@') || !email.value.includes('.com') || email.value.length < 10) {
+    console.log('Email inválido');
+    email.style.border = '2px solid red';
+  }
+
+  if (name.value.length < 3) {
+    console.log('Nome inválido');
+    name.style.border = '2px solid red';
+  }
+
+  if (surname.value.length < 3) {
+    console.log('Sobrenome inválido');
+    surname.style.border = '2px solid red';
+  }
+
+  if (username.value.length < 3) {
+    console.log('Usuário inválido');
+    username.style.border = '2px solid red';
+  }
+
+  if (password.value.length < 3) {
+    console.log('Senha inválida');
+    password.style.border = '2px solid red';
+  }
+}
+
+const form = document.querySelector('.form-auth');
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const { email, name, surname, username, password } = event.target.elements;
+
+  validate({ 
+    email: email, 
+    name: name,
+    surname: surname, 
+    username: username, 
+    password: password 
+  });
+}
+
