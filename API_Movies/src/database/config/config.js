11561acc4226793e7805e54e6fceca759f340f9d
Jessica Castro <jessica.castro_storm@prestador.globo>
2022-07-28 10:46:47 -0300

    Inserindo a API de filmes para utilização em aula

---
+++ API_Movies/src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "movies_admin",
+    "password": '',
+    "database": "movies_db",
+    "host": "192.168.205.3",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
